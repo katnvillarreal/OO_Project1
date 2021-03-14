35fb04e60bc41999674681e44e9cf1e210484f4b
katnv <katnvillarreal@gmail.com>
2021-03-14 16:56:49 -0500

    More BUTTONS

--- OO_Project/OO_Project/Main.java
+++ OO_Project/OO_Project/Main.java
@@ -1,10 +1,13 @@
 package OO_Project;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Push Counter");
+		JFrame frame = new JFrame("Weird Song Lookup");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		MusicPanel panel = new MusicPanel();
